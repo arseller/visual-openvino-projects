f237007c69a048d4c6bc941ae39fda001f55b983
LeonardoGuiducci <arseller@users.noreply.github.com>
2022-07-13 18:03:31 +0200

    requirements added, minor fixes

--- keras_to_savedmodel.py
+++ keras_to_savedmodel.py
@@ -1,6 +1,11 @@
+import os
 import tensorflow as tf
 
 model_name = 'glasses'
 
-model = tf.keras.models.load_model('models/keras_model.h5')
-tf.saved_model.save(model, 'model')
+model = tf.keras.models.load_model(f'models/{model_name}/keras/keras_model.h5')
+if not os.path.exists(f'models/{model_name}/converted_savedmodel'):
+    tf.saved_model.save(model, f'models/{model_name}')
+    print(f'model converted: models/{model_name}')
+else:
+    print(f'converted model already exists: models/{model_name}')
